de359fe4b588b42ef6bf13887e68a7f7502d98b1
kpkeller <kayleigh.keller@colostate.edu>
2024-09-23 15:19:06 -0600

    updating description with new reference.

--- R/spconf.R
+++ R/spconf.R
@@ -2,11 +2,5 @@
 #' @title Computing Scales of Spatial Smoothing for Confounding Adjustment
 #' @docType package
 #'
-#' @description Computes the effective range of a smoothing matrix,
-#' which is measure of the distance to which smoothing occurs.
-#' This is motivated by the application of spatial splines for adjusting
-#' for unmeasured spatial confounding in regression models, but the
-#' calculation of effective range can be applied to smoothing
-#' matrices in other contexts. For algorithmic details see
-#' <https://doi.org/10.1111/rssa.12556>
-NULL
+#' @keywords internal
+"_PACKAGE"
